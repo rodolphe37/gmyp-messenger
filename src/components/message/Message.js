e15acfb0e3ee506fb88e6150b615fcad73b1d093
rodolphe37 <rodolphe.a@gmail.com>
2020-09-17 15:29:53 +0200

    add timestamp

--- src/components/message/Message.js
+++ src/components/message/Message.js
@@ -7,7 +7,8 @@ function Message({ message, timestamp, user, userImage }) {
       <img src={userImage} alt="" />
       <div className="message_info">
         <h4>
-          {user} timestamp...
+          {user}
+          <span className="message_timestamp">{new Date(timestamp?.toDate()).toUTCString()}</span>
         </h4>
         <p>{message}</p>
       </div>
