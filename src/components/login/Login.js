43b3f1950d02d860f194c8954a1f305017762cd1
rodolphe37 <rodolphe.a@gmail.com>
2020-09-17 17:19:22 +0200

    use React Context API to access user from anywhere

---
+++ src/components/login/Login.js
@@ -0,0 +1,34 @@
+import { Button } from '@material-ui/core';
+import React from 'react';
+import './login.css';
+import { auth, provider } from '../../firebase'
+import { useStateValue } from '../../StateProvider';
+import { actionTypes } from '../../reducer';
+
+export default function Login() {
+  const [state, dispatch] = useStateValue();
+
+  const signIn = () => {
+    auth.signInWithPopup(provider).then(result => {
+      console.log(result)
+      dispatch({
+        type: actionTypes.SET_USER,
+        user: result.user,
+      })
+    })
+      .catch(error => {
+        alert(error.message)
+      })
+  }
+
+  return (
+    <div className="login">
+      <div className="login_container">
+        <img src={require('../../assets/images/logo.png')} alt='logo' />
+        <h1>Sign in to GmyP Messenger</h1>
+        <p>It's great!!</p>
+        <Button onClick={signIn}>Sign In With Google</Button>
+      </div>
+    </div>
+  )
+}
