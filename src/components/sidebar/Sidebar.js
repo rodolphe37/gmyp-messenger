c222aac9e2d11213de1d1a8617e55deb74b8f9bd
rodolphe37 <rodolphe.a@gmail.com>
2020-09-17 09:51:21 +0200

    add left sidebar options

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,7 +1,9 @@
 import FiberManualRecord from '@material-ui/icons/FiberManualRecord';
-import CreateIcon from '@material-ui/icons/Create'
+import CreateIcon from '@material-ui/icons/Create';
 import React from 'react';
 import './sidebar.css';
+import SidebarOption from '../sidebarOption/SidebarOption';
+import { Apps, ExpandLess, FileCopy, PeopleAlt, InsertComment, Inbox, Drafts, BookmarkBorder, ExpandMore, Add } from '@material-ui/icons';
 
 function Sidebar() {
   return (
@@ -16,6 +18,21 @@ function Sidebar() {
         </div>
         <CreateIcon />
       </div>
+      <SidebarOption Icon={InsertComment} title="threads" />
+      <SidebarOption Icon={Inbox} title="Mentions & reactions" />
+      <SidebarOption Icon={Drafts} title="Saved items" />
+      <SidebarOption Icon={BookmarkBorder} title="Channel browser" />
+      <SidebarOption Icon={PeopleAlt} title="People & user groups" />
+      <SidebarOption Icon={Apps} title="App" />
+      <SidebarOption Icon={FileCopy} title="File browser" />
+      <SidebarOption Icon={ExpandLess} title="Show less" />
+      <hr />
+      <SidebarOption Icon={ExpandMore} title="Channels" />
+      <hr />
+      <SidebarOption Icon={Add} addChannelOption={""} title="Add Channel" />
+
+      {/*Connect to db and list all the channels */}
+      {/*SidebarOption ... */}
     </div>
   )
 }
