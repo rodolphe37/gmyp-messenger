7c6631fb6300622f0ef21429ac9e7a9d2b4a179e
rodolphe37 <rodolphe.a@gmail.com>
2020-09-17 14:53:30 +0200

    use useParams (roomId) to fetch room details from the Database!

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import './chat.css';
 import StarBorderOutlinedIcon from '@material-ui/icons/StarBorderOutlined';
-import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined'
+import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
+import db from '../../firebase';
+
 
 function Chat() {
   const { roomId } = useParams();
+  const [roomDetails, setRoomDetails] = useState(null)
+  useEffect(() => {
+    if (roomId) {
+      db.collection('rooms').doc(roomId).onSnapshot(snapshot => (
+        setRoomDetails(snapshot.data())
+      ))
+    }
+  }, [roomId])
+  console.log(roomDetails)
+
   return (
     <div className="chat">
       <div className="chat_header">
         <div className="chat_headerLeft">
           <h4 className="chat_channelName">
-            <strong># general</strong>
+            <strong># {roomDetails?.name}</strong>
             <StarBorderOutlinedIcon />
           </h4>
         </div>
